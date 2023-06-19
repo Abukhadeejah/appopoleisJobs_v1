12caad10e8c60e0fe00dacc130641b558c71502f
Abukhadeejah <arsalanahmed82@hotmail.com>
2023-06-19 12:08:30 +0530

    Get All Jobs and Skills endpoint added

---
+++ appopoleisJobsBackend/job/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Job, Skill
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = '__all__'
--- appopoleisJobsBackend/job/views.py
+++ appopoleisJobsBackend/job/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import JobSerializer, SkillSerializer
+from .models import Job, Skill
 
 # Create your views here.
+@api_view(['GET'])
+def getAllJobs(request):
+
+    jobs = Job.objects.all()
+
+    serializer = JobSerializer(jobs, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getAllSkills(request):
+    skills = Skill.objects.all()
+    serializer = SkillSerializer(skills, many=True)
+    return Response(serializer.data)
+
