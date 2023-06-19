a52eeaed30f5b471100eb61bf8d3c266b6077026
Abukhadeejah <arsalanahmed82@hotmail.com>
2023-06-19 20:19:52 +0530

    add new Job and update job updated

--- appopoleisJobsBackend/job/urls.py
+++ appopoleisJobsBackend/job/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('jobs/', views.getAllJobs, name='jobs'),
+    path('jobs/new/', views.newJob, name='new_Job'),
     path('skills/', views.getAllSkills, name='skills'),
-    path('jobs/<slug:slug>/', views.getJob, name='job')
+    path('jobs/<slug:slug>/', views.getJob, name='job'),
+    path('jobs/<slug:slug>/update/', views.updateJob, name='update_job')
 
 ]
--- appopoleisJobsBackend/job/views.py
+++ appopoleisJobsBackend/job/views.py
@@ -30,3 +30,61 @@ def getJob(request, slug):
     job = get_object_or_404(Job, slug=slug)
     serializer = JobSerializer(job, many=False)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def newJob(request):
+    data = request.data
+
+    # Extract the skills data from the request data
+    skills_data = data.pop('skills', [])
+
+    # Create the job object
+    job = Job.objects.create(**data)
+
+    # Associate the skills with the job
+    for skill_data in skills_data:
+        skill = Skill.objects.get(pk=skill_data['id'])
+        job.skills.add(skill)
+
+    # Serialize the job object
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def updateJob(request, slug):
+
+    job = get_object_or_404(Job, slug=slug)
+
+    job.title = request.data['title']
+    job.description = request.data['description']
+    job.email = request.data['email']
+    job.location = request.data['location']
+    job.jobType = request.data['jobType']
+    job.industry = request.data['industry']
+    job.experience = request.data['experience']
+    job.salary = request.data['salary']
+    job.positions = request.data['positions']
+    job.company = request.data['company']
+    
+    # Update the skills
+    skills_data = request.data.get('skills', [])
+    job.skills.set(skills_data)
+
+    job.save()
+
+     # Serialize the job object
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+
+
+
+
+
+
+
+
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
